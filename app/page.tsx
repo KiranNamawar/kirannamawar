4b2e0f430728573e84ae305dcdb139ef794da405
Kiran Namawar <kirannamawar@gmail.com>
2024-05-02 11:58:43 +0530

    using Next.js Framework

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="text-2xl font-bold text-center">
+          Welcome to My Portfolio! I&apos;m Kiran Namawar, a self-taught web developer.
+        </p>
+      </div>
+    </main>
+  );
+}
